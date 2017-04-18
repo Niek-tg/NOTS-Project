a79f346f45afd3091cfedf95e607d05db32218f3
Sander Koenders <sanderkoenders@gmail.com>
2017-04-18 15:59:29 +0200

    Remving this

--- ethereum private chain new/server/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#GETH=./geth bash /gethcluster.sh ~/nodes 5 15452          05   77.160.58.3  -mine 
-GETH=./geth bash gethcluster.sh ~ 2 2 3301 05 77.160.58.3 -mine
-#GETH=./geth bash gethcluster.sh <root> <n> <network_id> <runid> <IP> [[params]...]
